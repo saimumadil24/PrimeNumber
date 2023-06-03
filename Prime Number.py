c829221e71dd34d76d2d4a33c32de4beafbea22e
Saimum Adil Khan <130060256+saimumadil24@users.noreply.github.com>
2023-06-03 06:04:10 +0600

    Add files via upload

---
+++ Prime Number.py
@@ -0,0 +1,12 @@
+def assignment2(x):
+    if x>1:
+        for i in range(2,x):
+            if (x%i)==0:
+                print(x," is not a prime number")
+                break
+        else:
+            print(x," is a prime number")
+    else:
+        print(x," is not a prime number")
+
+assignment2(59)
